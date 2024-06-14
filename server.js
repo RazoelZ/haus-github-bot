77620c770ef825154760c3e0bec10d758ce5ac83
RazoelZ <danydarmawannn@gmail.com>
2024-06-14 14:35:16 +0700

    third commit

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.post('/github-webhook', async (req, res) => {
     const payload = req.body;
 
